8c9e0c425b4da2e6d5317f89f0f025ac23afddf0
sharmeejaffer <sharmila.jafferalis@gmail.com>
2023-08-23 22:48:24 -0400

    Commit Day 2 Learnings

---
+++ JavaCourse/Day-2/JavaTutorial-2/src/JavaVariables.java
@@ -0,0 +1,32 @@
+
+public class JavaVariables {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		System.out.println(myNumber);
+		short myShort = 786;
+		
+		System.out.println(myShort);
+		long myLong = 7865;
+		
+		double myDouble = 5.7865;
+		float myFloat = 329.6f;
+		
+		char myChar = 'y';
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+	
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+		
+	
+				
+
+	}
+
+}
