8e92174a46717ae2aea0a1d11e780b6fd07051d9
sharmeejaffer <sharmila.jafferalis@gmail.com>
2023-09-04 19:57:25 -0400

    Commit - If Else

---
+++ JavaCourse/Day 6-2/ifelse/src/ifelse/ifelse1.java
@@ -0,0 +1,20 @@
+
+
+public class ifelse1 {
+	
+	public static void main(String[] args) {
+		
+		int myInt = 20;
+		
+		if(myInt < 10) {
+			System.out.println("yes, it's true");
+		}
+		else {
+			System.out.println("No, it's false");
+		}
+		
+		
+	}
+	
+
+}
