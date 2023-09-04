b93f8c9d30962619bed401da7bbb37d3587844b4
sharmeejaffer <sharmila.jafferalis@gmail.com>
2023-09-04 19:58:18 -0400

    Commit - Break statement

---
+++ JavaCourse/Day -7/Break/src/looping.java
@@ -0,0 +1,25 @@
+
+public class looping {
+
+  public static void main(String[] args) {
+  
+   int loop = 0;
+   
+   while(true) {
+	   System.out.println("Looping:" + loop);
+	    
+	    if(loop == 5); 
+	    break;
+   }
+    loop++;
+      System.out.println("Running");
+  }
+  }
+
+   
+   
+  
+
+	
+
+
