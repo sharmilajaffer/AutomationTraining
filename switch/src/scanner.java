2166f33b7a5c79f70b1fa9717b2a69d93a527883
sharmeejaffer <sharmila.jafferalis@gmail.com>
2023-09-04 22:31:19 -0400

    commit Switch statement

---
+++ switch/src/scanner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class scanner {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a command: ");
+		String text = input.nextLine();
+		
+		switch (text) {
+		case "start":
+			System.out.println("Machine started!");
+			break;
+		
+			
+		}
+			}
+
+}
